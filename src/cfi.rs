747d4dc53d90828dd9d109c01eab96c58a21aa40
main() <main@ehvag.de>
2017-10-21 17:04:05 +0200

    Discard DW_CFA_GNU_args_size

--- src/cfi.rs
+++ src/cfi.rs
@@ -3068,6 +3068,12 @@ impl<R: Reader> CallFrameInstruction<R> {
                 })
             }
 
+            // FIXME
+            constants::DW_CFA_GNU_args_size => {
+                let _ = input.read_uleb128()?;
+                Ok(CallFrameInstruction::Nop)
+            }
+
             otherwise => Err(Error::UnknownCallFrameInstruction(otherwise)),
         }
     }
